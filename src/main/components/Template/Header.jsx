19851182958d8a161f175b62d991d081f9cb0303
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-04-14 14:27:11 -0300

    Agora o Header é um componente funcional com React Hooks

--- src/main/components/Template/Header.jsx
+++ src/main/components/Template/Header.jsx
@@ -1,5 +1,5 @@
 import '../css/Header.css'
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import {
@@ -9,48 +9,43 @@ import {
     ListGroup,
 } from 'reactstrap'
 
-class Header extends React.Component {
-    state = {
-        modal: false
-    }
+const Header = props => {
+    const [openMenu, toggleMenu] = useState(false)
+    
+    const callToggleMenu = () => toggleMenu(!openMenu)
+    
+    return (
+        <>
+            <nav className="navbar fixed-top navbar-dark bg-warning ">
 
-    toggle = () => this.setState(prevState => ({ modal: !prevState.modal }))
-
-    render() {
-
-        return (
-            <>
-                <nav className="navbar fixed-top navbar-dark bg-warning ">
-
-                    <div className="mx-auto flex-grow-1">
-                        <div className="container-fluid">
-                            <div className="row">
-                                <div className="col-6 col-sm-10 offset-sm-1 text-center">
-                                    <h1 className="brand-title">Randomizador</h1>
-                                </div>
-                                <div className="col-6 col-sm-1">
-                                    <button
-                                        onClick={this.toggle}
-                                        className="btn btn-link btn-warning float-right">
-                                        <span className="navbar-toggler-icon"></span>
-                                    </button>
-                                </div>
+                <div className="mx-auto flex-grow-1">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-6 col-sm-10 offset-sm-1 text-center">
+                                <h1 className="brand-title">Randomizador</h1>
+                            </div>
+                            <div className="col-6 col-sm-1">
+                                <button
+                                    onClick={callToggleMenu}
+                                    className="btn btn-link btn-warning float-right">
+                                    <span className="navbar-toggler-icon"></span>
+                                </button>
                             </div>
                         </div>
                     </div>
-                </nav>
-                <Modal isOpen={this.state.modal} toggle={this.toggle} centered size="sm">
-                    <ModalHeader className="bg-warning text-center text-light">Menu</ModalHeader>
-                    <ModalBody className="">
-                        <ListGroup className="text-center">
-                            <Link to="/" className="list-group-item list-group-item-action">Home</Link>
-                            <Link to="/about" className="list-group-item list-group-item-action" >About</Link>
-                        </ListGroup>
-                    </ModalBody>
-                </Modal>
-            </>
-        )
-    }
+                </div>
+            </nav>
+            <Modal isOpen={openMenu} toggle={callToggleMenu} centered size="sm">
+                <ModalHeader className="bg-warning text-center text-light">Menu</ModalHeader>
+                <ModalBody className="">
+                    <ListGroup className="text-center">
+                        <Link to="/" className="list-group-item list-group-item-action">Home</Link>
+                        <Link to="/about" className="list-group-item list-group-item-action" >About</Link>
+                    </ListGroup>
+                </ModalBody>
+            </Modal>
+        </>
+    )
 }
 
 export default Header
