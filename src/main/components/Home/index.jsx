d41f6d8d0de75ac6e980d5c41591c5629ebd076b
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-04-20 12:15:55 -0300

    Netlify setup e iniciada a criação da tela inicial

--- src/main/components/Home/index.jsx
+++ src/main/components/Home/index.jsx
@@ -1,14 +1,50 @@
 import React from 'react'
 import Template from '../Template/'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <Template>
             <div className="section">
-                <div className="container">
+                <div className="jumbotron mt-3">
                     <div className="row">
-                        <div className="col">
-                            <h1>Home</h1>
+                        <div className="col-md-8 offset-md-2 text-center">
+                            <h1>Bem-vindo ao Randomizador!</h1>
+                            <h2>Faça login para salvar suas listas, sorteios, além de utilizar as ferramentas integradas com as redes sociais</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6 offset-3">
+                            <Link to="/login" className="btn btn-primary btn-block">Entrar</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="section">
+                <div className="jumbotron">
+                    <div className="row">
+                        <div className="col-md-10 offset-md-2">
+                            <div className="row">
+                                <div className="col-md-1 text-center">
+                                    <p className="h2">Sortear</p>
+                                </div>
+                                <div className="col-md-2 text-center">
+                                    <input className="form-control" type="number" />
+                                </div>
+                                <div className="col-md-2 text-center">
+                                    <p className="h2">número entre</p>
+                                </div>
+                                <div className="col-md-2 text-center">
+                                    <input className="form-control" type="number" />
+                                </div>
+                                <div className="col-md-1 text-center">
+                                    <p className="h2">e</p>
+                                </div>
+                                <div className="col-md-3 text-center">
+                                    <input className="form-control" type="number" />
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/main/components/Login/index.jsx
+++ src/main/components/Login/index.jsx
@@ -11,14 +11,9 @@ const Login = () => {
 
     const [isLoadingUi, setIsLoadingUi] = useState(true)
     const [isSigningInDone, setSigningAsDone] = useState(false)
-    const [curUser, setCurUser] = useState({})
 
     useEffect(() => {
         initializeFirebaseUi()
-        if (firebase.auth().currentUser) {
-            setCurUser(firebase.auth().currentUser)
-            console.log(firebase.auth().currentUser)
-        }
     }, [])
 
     const signInSuccessful = (authResult, resirectUrl) => {
@@ -44,50 +39,21 @@ const Login = () => {
 
     return (
         <>
-            {/* <If c={isSigningInDone}>
+            <If c={isSigningInDone}>
                 <Redirect to="/" />
             </If>
-            <If c={!isSigningInDone}> */}
-            <Template>
-                <div id="firebaseui-auth-container" className={isLoadingUi ? 'invisible' : ''}></div>
-                <If c={isLoadingUi}>
-                    <Row>
-                        <Col className="pl-0" xs={{ size: 6, offset: 6 }}>
-                            <Spinner className="mx-0 px-0" color="warning" />
-                        </Col>
-                    </Row>
-                </If>
-                <p>
-                    <span className="text-primary text-bold">uid: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.uid : ''}</span>
-                </p>
-                <p>
-                    <span className="text-primary text-bold">emailVerified: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.emailVerified ? 'true' : 'false' : ''}</span>
-                </p>
-                <p>
-                    <span className="text-primary text-bold">uid: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.uid : ''}</span>
-                </p>
-                <p>
-                    <span className="text-primary text-bold">displayName: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.displayName : ''}</span>
-                </p>
-                <p>
-                    <span className="text-primary text-bold">email: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.email : ''}</span>
-                </p>
-                <p>
-                    <span className="text-primary text-bold">photoUrl: </span><span className="text-secondary">{firebase.auth().currentUser ? firebase.auth().currentUser.photoUrl : ''}</span>
-                </p>
-                <button className="btn btn-outline-primary" onClick={() => {
-                    
-                     let currentUser = firebase.auth().currentUser
-                     if (currentUser) {
-                         currentUser.sendEmailVerification().then(() => {
-                             console.log('confirmação enviada')
-                         })
-                     }else{
-                         alert('usuario não encontrado')
-                     }
-                }}>Send</button>
-            </Template>
-            {/* </If> */}
+            <If c={!isSigningInDone}>
+                <Template>
+                    <div id="firebaseui-auth-container" className={isLoadingUi ? 'invisible' : ''}></div>
+                    <If c={isLoadingUi}>
+                        <Row>
+                            <Col className="pl-0" xs={{ size: 6, offset: 6 }}>
+                                <Spinner className="mx-0 px-0" color="warning" />
+                            </Col>
+                        </Row>
+                    </If>
+                </Template>
+            </If>
         </>
     )
 
