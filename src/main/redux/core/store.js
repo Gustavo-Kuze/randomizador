23fd770512e3861d63dd3d84ebf424bff986e5c8
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-04-21 14:09:31 -0300

    Update store.js

--- src/main/redux/core/store.js
+++ src/main/redux/core/store.js
@@ -5,8 +5,6 @@ import createSaga from 'redux-saga'
 
 const sagaMiddleware = createSaga()
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-export default createStore(combinedReducers, compose(applyMiddleware(sagaMiddleware), devTools))
+export default createStore(combinedReducers, compose(applyMiddleware(sagaMiddleware)))
 
 sagaMiddleware.run(sagas)
