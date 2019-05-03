649612bd1ed4e2a09c71d4c59e3f8671d0585772
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-05-03 11:36:25 -0300

    Iniciada a criação do save publico

--- src/main/components/draw/subcomponents/DrawResults.jsx
+++ src/main/components/draw/subcomponents/DrawResults.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Tooltip } from 'reactstrap'
 
 const DrawResults = props => {
+    const [isTooltipOpen, toggleTooltip] = useState()
+
     return (
         <>
             <div className="row mt-5">
                 <div className={props.colClasses || 'col-12'} >
-                    <h1 className={props.h1Classes || 'text-center lobster'}>{props.title || 'Confira os resultados'}</h1>
+                    <h1 className={props.titleClasses || 'text-center lobster'}>{props.title || 'Confira os resultados'}</h1>
                 </div>
             </div>
             <div className="row">
@@ -16,6 +19,17 @@ const DrawResults = props => {
             <div className="row mt-5">
                 <div className={props.colClasses || 'col-12'}>
                     <h4 className="text-center sofia">Sorteio realizado em: {props.date || new Date().toLocaleString()}</h4>
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className={'col-12 col-sm-6 my-1'}>
+                    <Tooltip placement="bottom" isOpen={isTooltipOpen} target="btn-save-publicly" toggle={() => toggleTooltip(!isTooltipOpen)}>
+                        <p>O resultado ficará acessível para aquele que obtiver o número, para todo o sempre</p>
+                    </Tooltip>
+                    <button id="btn-save-publicly" className="btn btn-block btn-outline-warning">Salvar publicamente</button>
+                </div>
+                <div className={'col-12 col-sm-6 my-1'}>
+                    <button className="btn btn-block btn-outline-info">Salvar privado</button>
                 </div>
             </div>
         </>
---
+++ src/main/services/firebase/publicDraws.js
@@ -0,0 +1,24 @@
+import firebase from './index'
+
+let drawsRef = firebase.firestore()
+    .collection('public_results')
+
+const _getPublicResultsCount = async () => {
+    let snap = await drawsRef.get()
+    return snap.size
+}
+
+const savePublicResult = async (drawResults) => {
+    let currentCount = await _getPublicResultsCount()
+    await drawsRef.doc(`${currentCount + 1}`).set(drawResults)
+    return currentCount + 1
+}
+
+const getPublicResult = async (resultNumber) => {
+    let snap = await drawsRef.doc(resultNumber).get()
+    return { id: snap.id, ...snap.data() }
+}
+
+export {
+    savePublicResult, getPublicResult
+}
