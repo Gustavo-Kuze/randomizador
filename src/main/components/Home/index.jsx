1bb514e654351576faf4c492e368d70a826a6f9d
Gustavo Kuze <gustavo.silva@rcadigital.com.br>
2019-05-24 14:15:42 -0300

    Adicionados avisos à tela inicial e ao README

--- src/main/components/Home/index.jsx
+++ src/main/components/Home/index.jsx
@@ -10,6 +10,12 @@ const Home = () => {
         <Template>
             <div className="section">
                 <div className="container-fluid mb-5">
+                    <div className="row jumbotron">
+                        <div className="col">
+                            <p className="h2"> <strong className="text-danger"><span role="img" aria-label="Atenção">⚠</span> ATENÇÃO</strong></p>
+                            <p className="h3"> Este app se encontra em DESENVOLVIMENTO! Todos os dados aqui salvos, podem ser excluídos a qualquer momento. Os sorteios do Facebook e Instagram precisam passar pela aprovação do Facebook e não estão 100% funcionais. Agradecemos sua paciência e compreensão!</p>
+                        </div>
+                    </div>
                     <div className="row my-3">
                         <div className="col-6 offset-3 d-flex justify-content-center align-items-center">
                             <img src="/img/randomizador_icon_1024.png" alt="Logo" className="img-fluid" style={{ maxHeight: '200px' }} />
