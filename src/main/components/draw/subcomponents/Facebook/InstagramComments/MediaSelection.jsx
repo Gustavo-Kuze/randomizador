d021d4288e6e262b78eca000cdc7827f8ec187d5
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-05-25 18:57:12 -0300

    Correção na numeração do sorteio do Instagram

--- src/main/components/draw/subcomponents/Facebook/InstagramComments/MediaSelection.jsx
+++ src/main/components/draw/subcomponents/Facebook/InstagramComments/MediaSelection.jsx
@@ -30,7 +30,7 @@ const MediaSelection = (props) => {
         <>
             <button className={`btn btn-outline-info btn-block text-left mt-3 ${props.enabled ? '' : 'disabled'}`}
                 disabled={!props.enabled}
-                onClick={() => props.setIsOpen(props.enabled && !props.isOpen)}>3- Escolher o post</button>
+                onClick={() => props.setIsOpen(props.enabled && !props.isOpen)}>2- Escolher o post</button>
             <Collapse isOpen={props.enabled && props.isOpen}>
                 <div className="card p-5 my-3">
                     {
