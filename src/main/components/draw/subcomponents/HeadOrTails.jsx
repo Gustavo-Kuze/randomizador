45d1cfa4c4d358801b889d39e88d85248a0c4604
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2019-05-25 16:40:43 -0300

    HeadOrTails agora é um class component e os timeouts são limpos no unmount

--- src/main/components/draw/subcomponents/HeadOrTails.jsx
+++ src/main/components/draw/subcomponents/HeadOrTails.jsx
@@ -1,75 +1,90 @@
 import '../../css/HeadOrTails.css'
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 import If from '../../utils/If'
 import DrawResults from '../subcomponents/DrawResults'
 import drawTypes from '../drawUtils/drawTypes'
 
-const HeadOrTails = () => {
+class HeadOrTails extends Component {
+    constructor(props) {
+        super(props)
+        this.drawTimeOut = null
+        this.resultsTimeOut = null
+    }
 
-    const [showResult, setShowResult] = useState(false)
-    const [headOrTails, setHeadOrTails] = useState('')
+    state = {
+        showResult: false,
+        headOrTails: ''
+    }
 
-    const flip = () => {
+    componentWillUnmount = () => {
+        clearTimeout(this.drawTimeOut)
+        clearTimeout(this.resultsTimeOut)
+    }
+
+    flip = () => {
         const moeda = document.getElementById('moeda')
-        setShowResult(false)
+        this.setState({ ...this.state, showResult: false })
         let result = Math.random();
         moeda.className = ''
 
-        setTimeout(() => {
+        this.drawTimeOut = setTimeout(() => {
             if (result <= 0.5) {
                 moeda.classList.add('gira-cara')
-                setHeadOrTails('Cara!')
+                this.setState({ ...this.state, headOrTails: 'Cara!' })
             }
             else {
                 moeda.classList.add('gira-coroa')
-                setHeadOrTails('Coroa!')
+                this.setState({ ...this.state, headOrTails: 'Coroa!' })
             }
         }, 15);
 
-        setTimeout(() => {
-            setShowResult(true)
+        this.resultsTimeOut = setTimeout(() => {
+            this.setState({ ...this.state, showResult: true })
         }, 3000);
     }
 
-    return (
-        <>
-            <div className="section">
-                <div className="container">
-                    <div className="row mb-5">
-                        <div className="col">
-                            <h1 className="sofia"><strong>Cara ou Coroa</strong></h1>
+    render() {
+        return (
+            <>
+                <div className="section">
+                    <div className="container">
+                        <div className="row mb-5">
+                            <div className="col">
+                                <h1 className="sofia"><strong>Cara ou Coroa</strong></h1>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h1 className="h3 text-center mb-5">Clique na moeda para jogar</h1>
+                            </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col">
-                            <h1 className="h3 text-center mb-5">Clique na moeda para jogar</h1>
-                        </div>
+                </div>
+                <div id="moeda" onClick={this.flip}>
+                    <div className="cara">
+                        <img src="/img/cara.png" alt="Cara" />
+                    </div>
+                    <div className="coroa">
+                        <img src="/img/coroa.png" alt="Coroa" />
                     </div>
                 </div>
-            </div>
-            <div id="moeda" onClick={flip}>
-                <div className="cara">
-                    <img src="/img/cara.png" alt="Cara" />
+                <div className="section mt-5">
+                    <div className="container">
+                        <If c={this.state.showResult}>
+                            <DrawResults title="O resultado foi:"
+                                date={`${new Date().toLocaleString()}`}
+                                drawType={drawTypes.HEAD_OR_TAILS}
+                                result={this.state.headOrTails}
+                            >
+                                <h3 className="display-4 text-center my-5 lobster h2 text-weight-bold">{this.state.headOrTails}</h3>
+                            </DrawResults>
+                        </If>
+                    </div>
                 </div>
-                <div className="coroa">
-                    <img src="/img/coroa.png" alt="Coroa" />
-                </div>
-            </div>
-            <div className="section mt-5">
-                <div className="container">
-                    <If c={showResult}>
-                        <DrawResults title="O resultado foi:"
-                            date={`${new Date().toLocaleString()}`}
-                            drawType={drawTypes.HEAD_OR_TAILS}
-                            result={headOrTails}
-                        >
-                            <h3 className="display-4 text-center my-5 lobster h2 text-weight-bold">{headOrTails}</h3>
-                        </DrawResults>
-                    </If>
-                </div>
-            </div>
-        </>
-    )
+            </>
+        )
+    }
+
 }
 
 export default HeadOrTails
